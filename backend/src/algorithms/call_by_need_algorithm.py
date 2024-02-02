fd7d27eb08a4943ee14d51e6cd87d061daa3a50a
Tom Wawerek <wawerektom@gmail.com>
2024-02-02 11:04:03 +0100

    FIX: backend work

--- backend/src/algorithms/call_by_need_algorithm.py
+++ backend/src/algorithms/call_by_need_algorithm.py
@@ -1,23 +1,26 @@
-import numpy as np
 from pydantic import BaseModel
 
 from .base_algorithm import BaseAlgorithm
+from .pebbling_algorithm import PebblingAlgorithm
 
 
 class Edge(BaseModel):
     label: int  # Label describing the relationship or action
     head: str  # Identifier for the head node this edge points to
 
+
 class Node(BaseModel):
-    val: bool  = False # Evaluation outcome of the node
-    computed: bool  = False # Tracks if the node has been computed
-    susp: bool  = False # Tracks if the node is being evaluated (is suspended)
-    loop: bool  = False # Tracks if the node is part of a loop
-    edges: list[Edge] = [] # List of edges originating from this node
+    val: bool = False  # Evaluation outcome of the node
+    computed: bool = False  # Tracks if the node has been computed
+    susp: bool = False  # Tracks if the node is being evaluated (is suspended)
+    loop: bool = False  # Tracks if the node is part of a loop
+    edges: list[Edge] = []  # List of edges originating from this node
+
 
 class CallByNeedState(BaseModel):
     nodes: dict[str, Node]  # Dictionary of nodes, with node labels as keys
     is_satisfiable: bool | None = None
+
 
 class CallByNeedAlgorithm(BaseAlgorithm):
     name = "Call-By-Need Algorithm"
@@ -25,73 +28,84 @@ class CallByNeedAlgorithm(BaseAlgorithm):
     def _initialize(self):
         """Initialize the state for the algorithm."""
 
-        #initialize facts
+        # Initialize Nodes
         self.nodes = {str(i): Node() for i in range(self.formula.num_literals)}
-        self.nodes['false'] = Node()
-        self.nodes['true'] = Node(val=True, computed=True)
-        for fact_clause_index in self.formula.facts:
-            fact = str(self.formula.heads[fact_clause_index])
-            #self.nodes[fact] = Node()
-            self.nodes[fact].val = True
-            self.nodes[fact].computed = True
-            self.nodes[fact].edges = [Edge(label=fact_clause_index, head='true')] #add edge to true node
-        
-        for clause_index, head in enumerate(self.formula.heads):
-            tail = str(head) if head >=0 else 'false'
+        self.nodes["false"] = Node()
+        self.nodes["true"] = Node(val=True, computed=True)
 
-            if tail not in self.nodes:
-                self.nodes[tail] = Node()
+        # Initialize Facts
+        for idx in self.formula.facts:
+            head = str(self.formula.heads[idx])
+            self.nodes[head].val = True
+            self.nodes[head].computed = True
+            self.nodes[head].edges = [Edge(label=idx, head="true")]  # add edge to true node
 
-            for i, literal in enumerate(self.formula.matrix[clause_index]):
-                if literal:
-                    self.nodes[tail].edges.append(Edge(label=clause_index, head=str(i)))
-                
+        # Initialize Edges
+        for idx, clause in enumerate(self.formula.matrix):
+            head = self.formula.heads[idx]
+            source = str(head) if head >= 0 else "false"
 
+            for i, literal in enumerate(clause):
+                if not literal or i == head:
+                    continue
+
+                self.nodes[source].edges.append(Edge(label=idx, head=str(i)))
 
     @property
     def state(self) -> CallByNeedState:
-        return CallByNeedState(
-            nodes = self.nodes
-        )
+        return CallByNeedState(nodes=self.nodes)
 
-    def bottom_up_visit(self, start_node):
-        # Placeholder for visited nodes to avoid re-evaluation
-        visited = set()
+    def bottom_up_visit(self, p):
+        node = self.nodes[p]
+        subgraph = {node}
 
-        def visit(node_id):
-            if node_id in visited:
-                return
-            visited.add(node_id)
-
-            node = self.nodes[node_id]
+        def rec_subgraph(node):
             for edge in node.edges:
-                if edge.head not in visited:
-                    visit(edge.head)
+                if edge.head not in subgraph:
+                    subgraph.add(edge.head)
+                    rec_subgraph(self.nodes[edge.head])
 
-            # After visiting all dependencies, evaluate the node if not already computed
-            if not node.computed:
-                self.evalue(node_id)
+        rec_subgraph(node)
+
+        sub_formula = subgraph
+
+        pebbling = PebblingAlgorithm(self.formula)
+        gen = pebbling.run_step()
+
+        # return result of pebbling algorithm (state.is_satisfiable)
+        # last step of generator is the result
+        while True:
+            try:
+                next(gen)
+            except StopIteration as e:
+                break
+        
+        self.is_satisfiable = pebbling.is_satisfiable
+        self.nodes[start_node].val = not pebbling.is_satisfiable
+        self.nodes[start_node].computed = True
+        
+
     def evalue(self, p):
         node = self.nodes[p]
         node.susp = True
         tagset = {edge.label for edge in node.edges}
 
-        for i in tagset:
-            if not node.val:
+        if not node.val:
+            for i in tagset:
                 arc = [edge for edge in node.edges if edge.label == i]
-                node_set = {edge.head for edge in arc}
-                temp = self.minnode(node_set)
+                node_set = [edge.head for edge in arc]
 
+                temp = self.minnode(node_set)
                 if temp:
                     node.val = True
                     if node.loop:
                         self.bottom_up_visit(p)
 
-
         node.susp = False
         node.loop = False
         node.computed = True
         yield self.state
+
         return node.val
 
     def minnode(self, nodes):
@@ -101,14 +115,13 @@ class CallByNeedAlgorithm(BaseAlgorithm):
             if node.computed:
                 if not node.val:
                     number += 1
-            else:
-                if not node.susp:
-                    temp = self.evalue(j)
-                    if not temp:
-                        number += 1
-                else:
-                    node.loop = True
+            elif not node.susp:
+                temp = self.evalue(j)
+                if not temp:
                     number += 1
+            else:
+                node.loop = True
+                number += 1
 
         return number == 0
 
@@ -116,7 +129,7 @@ class CallByNeedAlgorithm(BaseAlgorithm):
         self._initialize()
         yield self.state
 
-        temp = self.evalue('false')
+        temp = self.evalue("false")
         self.is_satisfiable = not temp
 
         yield self.state
--- frontend/src/visualization/call_by_need/CallByNeedVisualization.tsx
+++ frontend/src/visualization/call_by_need/CallByNeedVisualization.tsx
@@ -4,22 +4,21 @@ import React, { useMemo } from "react";
 import Graph, { Edge, Node, Options } from "react-graph-vis";
 
 interface AlgorithmEdge {
-  label: number; // Label describing the relationship or action
-  head: string; // Identifier for the head node this edge points to
+	label: number; // Label describing the relationship or action
+	head: string; // Identifier for the head node this edge points to
 }
 
 interface AlgorithmNode {
-  val: boolean; // Evaluation outcome of the node
-  computed: boolean; // Tracks if the node has been computed
-  susp: boolean; // Tracks if the node is being evaluated (is suspended)
-  loop: boolean; // Tracks if the node is part of a loop
-  edges: AlgorithmEdge[]; // List of edges originating from this node
+	val: boolean; // Evaluation outcome of the node
+	computed: boolean; // Tracks if the node has been computed
+	susp: boolean; // Tracks if the node is being evaluated (is suspended)
+	loop: boolean; // Tracks if the node is part of a loop
+	edges: AlgorithmEdge[]; // List of edges originating from this node
 }
 
 interface CallByNeedRunState extends BaseAlgorithmState {
-  nodes: { [key: string]: AlgorithmNode }; // Dictionary of nodes, with node labels as keys
+	nodes: { [key: string]: AlgorithmNode }; // Dictionary of nodes, with node labels as keys
 }
-
 
 const CallByNeedVisualization: React.FC = () => {
 	const { data } = useAlgorithm<CallByNeedRunState>();
@@ -59,7 +58,7 @@ const CallByNeedVisualization: React.FC = () => {
 
 // Convert the current state of the Pebbling Algorithm and formula to a graph format
 const convertToGraph = (
-		data: CallByNeedRunState
+	data: CallByNeedRunState
 ): { nodes: Node[]; edges: Edge[] } => {
 	// Create a node for each literal in the formula
 	const nodes: Node[] = [];
@@ -68,7 +67,7 @@ const convertToGraph = (
 	const addNode = (id: string, color: string) => {
 		nodes.push({
 			id,
-			label: id === 'true' || id === 'false' ? id : `P${id}`,
+			label: id === "true" || id === "false" ? id : `P${id}`,
 			color: color,
 			font: { color: "white" },
 		});
@@ -76,26 +75,21 @@ const convertToGraph = (
 
 	// Create nodes for each literal in the formula
 	Object.entries(data.nodes).forEach(([id, node]) => {
-		if (node.val && node.computed){
+		if (node.val && node.computed) {
 			addNode(id, "green");
-		}
-		else if (node.susp){
+		} else if (node.susp) {
 			addNode(id, "blue");
-		}
-		else if (node.loop){
+		} else if (node.loop) {
 			addNode(id, "yellow");
-		}
-		else if (!node.val && node.computed){
+		} else if (!node.val && node.computed) {
 			addNode(id, "red");
-		}
-		else {
+		} else {
 			addNode(id, "grey");
 		}
-
 	});
 
-
-	/* 
+	// Create a map to consolidate edges by source and target
+	const edgeMap = new Map<string, Edge>();
 	// Add an edge to the map, combining edges with the same source and target
 	const addEdge = (source: string, target: string, clause: number) => {
 		const key = `${source}-${target}`;
@@ -114,40 +108,29 @@ const convertToGraph = (
 			edge.label += `, ${clause}`;
 		}
 
-		// Highlight edges that are active or dependent
-		if (clause === dependant_clause) {
-			edge.color = "blue";
-		} else if (numargs[clause] === 0 && edge.color === "black") {
-			edge.color = "green";
-		}
+		// // Highlight edges that are active or dependent
+		// if (clause === dependant_clause) {
+		// 	edge.color = "blue";
+		// } else if (numargs[clause] === 0 && edge.color === "black") {
+		// 	edge.color = "green";
+		// }
 
 		edgeMap.set(key, edge);
 	};
 
 	// Add edges from each literal to its head
-	formula.matrix.forEach((clause, index) => {
-		const head = formula.heads[index];
-		clause.forEach((used, lit) => {
-			if (used && lit !== head) {
-				addEdge(lit, head, index);
-			}
+	Object.entries(data.nodes).forEach(([id, node]) => {
+		node.edges.forEach((edge) => {
+			addEdge(id, edge.head, edge.label);
 		});
-
-		// Add edges from the True node to all clauses that are facts (no negative literals)
-		if (clause.every((used, lit) => !used || lit === head)) {
-			addEdge(-2, head, index);
-		}
 	});
 
 	// Convert edgeMap values to an array
 	const edges: Edge[] = Array.from(edgeMap.values());
-	*/
-
-	const edges_list: Edge[] = [];
 
 	return {
 		nodes,
-		edges: edges_list,
+		edges,
 	};
 };
 
